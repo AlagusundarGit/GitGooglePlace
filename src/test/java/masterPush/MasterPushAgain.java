e278c8098fb12abcb4246070b708e312c61ff2ce
AS00892314 <as00892314@techmahindra.com>
2024-11-10 21:26:33 +0530

    Master repush:

---
+++ src/test/java/masterPush/MasterPushAgain.java
@@ -0,0 +1,13 @@
+package masterPush;
+
+import org.junit.Test;
+
+public class MasterPushAgain {
+@Test
+private void pu() {
+	// TODO Auto-generated method stub
+	
+	System.out.println("Master");
+
+}
+}
