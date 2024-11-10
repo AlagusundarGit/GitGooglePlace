e2f8a211f0ad677daaa212e3a6de60b201247a07
AS00892314 <as00892314@techmahindra.com>
2024-11-10 08:06:28 +0530

    Restored

--- src/test/java/gitPakage/GitTestClass.java
+++
@@ -1,7 +0,0 @@
-package gitPakage;
-
-public class GitTestClass {
-	public static void main(String[] args) {
-		System.out.println("TestClass");
-	}
-}
--- src/test/java/gitTest/GTestClass.java
+++
@@ -1,7 +0,0 @@
-package gitTest;
-
-public class GTestClass {
-	public static void main(String[] args) {
-		System.out.println("Test class git");
-	}
-}
